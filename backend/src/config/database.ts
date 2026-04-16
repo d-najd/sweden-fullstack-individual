bc816785134ae21e15a40a5b2691c871e048b14b
d-najd <dimitar.hristijan.najdovski@gmail.com>
2026-04-16 14:57:51 +0200

    build: updating .env

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,10 +1,7 @@
 import envConfig from "./env"
 import { MongoClient, ServerApiVersion } from "mongodb"
 
-const uri = `mongodb://${envConfig.username}:${envConfig.password}@ac-goxif1i-shard-00-00.7k3tg9d.mongodb.net:27017,ac-goxif1i-shard-00-01.7k3tg9d.mongodb.net:27017,ac-goxif1i-shard-00-02.7k3tg9d.mongodb.net:27017/?ssl=true&replicaSet=atlas-3hakrn-shard-0&authSource=admin&appName=sweden-fullstack-individual`
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+const client = new MongoClient(envConfig.databaseUrl!, {
 	serverApi: {
 		version: ServerApiVersion.v1,
 		strict: true,
@@ -14,15 +11,10 @@ const client = new MongoClient(uri, {
 
 async function run() {
 	try {
-		// Connect the client to the server	(optional starting in v4.7)
 		await client.connect()
-		// Send a ping to confirm a successful connection
 		await client.db(envConfig.database).command({ ping: 1 })
-		console.log(
-			"Pinged your deployment. You successfully connected to MongoDB!",
-		)
+		console.log("Successfully connected to MongoDB")
 	} catch {
-		// Ensures that the client will close when you finish/error
 		await client.close()
 	}
 }
--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -11,15 +11,9 @@ init()
 
 const envConfig = {
 	port: process.env.BACKEND_PORT ? parseInt(process.env.BACKEND_PORT) : 3000,
-	databaseHost: process.env.DB_HOST ? process.env.DB_HOST : "localhost",
+	databaseUrl: process.env.DB_URL,
 	database: process.env.DB_DATABASE
 		? process.env.DB_DATABASE
-		: "sweden-backend",
-	username: process.env.DB_USERNAME
-		? process.env.DB_USERNAME
-		: "sweden-backend",
-	password: process.env.DB_PASSWORD
-		? process.env.DB_PASSWORD
 		: "sweden-backend",
 }
 
