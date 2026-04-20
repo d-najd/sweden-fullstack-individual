d592f63f3e66a653027ff12dd1d91ef1409d410a
d-najd <dimitar.najdovskiw@gmail.com>
2026-04-21 00:15:03 +0200

    format: fixing format and lint with format and lint commands

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite"
 import path from "path"
 import dotenv from "dotenv"
 
@@ -26,10 +26,7 @@ export default defineConfig({
 			},
 		},
 	},
-	plugins: [
-      react(),
-      tailwindcss()
-   ],
+	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
