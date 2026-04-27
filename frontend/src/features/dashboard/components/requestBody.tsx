a64b8896c2b7a00f58dc1d8663945ad630f45bad
d-najd <59766732+d-najd@users.noreply.github.com>
2026-04-27 14:35:51 +0200

    style(frontend): reworked the screen with resizable (#14)

--- frontend/src/features/dashboard/components/requestBody.tsx
+++ frontend/src/features/dashboard/components/requestBody.tsx
@@ -1,23 +1,26 @@
 import { Textarea } from "@/components/ui/textarea"
 import useSelectedRequestStore from "../stores/selectedRequestStore"
 
-function RequestBody() {
+function RequestBody({ className }: React.ComponentProps<"div">) {
 	const { selectedRequest, setSelectedRequest } = useSelectedRequestStore()
 
 	return (
-		<Textarea
-			value={selectedRequest?.json_body ?? ""}
-			onChange={(e) => {
-				if (!selectedRequest) return
+		<div className={className}>
+			<Textarea
+				className="h-full outline-hidden! ring-0!"
+				value={selectedRequest?.json_body ?? ""}
+				onChange={(e) => {
+					if (!selectedRequest) return
 
-				setSelectedRequest({
-					...selectedRequest,
-					json_body:
-						e.target.value !== "" ? e.target.value : undefined,
-				})
-			}}
-			rows={12}
-		></Textarea>
+					setSelectedRequest({
+						...selectedRequest,
+						json_body:
+							e.target.value !== "" ? e.target.value : undefined,
+					})
+				}}
+				rows={12}
+			/>
+		</div>
 	)
 }
 
